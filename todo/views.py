7edd1c19a005d1f8d834c3127cd73a00841c2089
SivakalimuthuR <sivakalimuthu07@gmail.com>
2024-06-28 12:01:07 +0530

    todo

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,6 @@ from todo.models import Task
 def home (request):
     tasks = Task.objects.filter(is_completed=True)
     context={
-        'task':tasks,
+        'tasks':task,
     }
     return render (request,'home.html',context)
